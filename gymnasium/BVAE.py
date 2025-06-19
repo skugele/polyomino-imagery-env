0723a60e869a89d099ccc884417b7ef2dadc3f43
nao <theshishir01@gmail.com>
2025-06-19 16:45:35 -0500

    testing changes

---
+++ gymnasium/BVAE.py
@@ -0,0 +1,285 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from time import time
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Lambda, Reshape, Conv2DTranspose, BatchNormalization
+
+# PARAMETERS
+INPUT_HEIGHT = 128
+INPUT_WIDTH = 128
+
+INPUT_SHAPE = (INPUT_HEIGHT, INPUT_WIDTH, 1)
+
+latent_dims = 16
+
+class Preprocessor(tf.keras.Model):
+  def __init__(self):
+    super().__init__(name="preprocessor")
+    self.resizer = tf.keras.layers.Resizing(INPUT_HEIGHT, INPUT_WIDTH, name=f"{self.name}_resizer")
+    self.rescaler = tf.keras.layers.Rescaling(scale=1./255, name=f"{self.name}_rescaler")
+
+  def call(self, inputs):
+    return self.rescaler(
+        self.resizer(inputs)
+    )
+
+
+class Encoder(tf.keras.layers.Layer):
+  def __init__(self, latent_dims):
+    super().__init__(name="encoder")
+
+    self.latent_dims = latent_dims
+
+    # layers
+    self.preprocess = Preprocessor()
+    # 128 x 128 x 1
+    self.conv1 = Conv2D(filters=16, kernel_size=3, strides=1, padding='same', activation='relu', name='encoder_conv1')
+    self.bnorm1 = BatchNormalization(name="encoder_bnorm1")
+    self.mpool1 = MaxPool2D(name='encoder_mpool1')
+
+    # 64 x 64 x 16
+    self.conv2 = Conv2D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu', name='encoder_conv2')
+    self.bnorm2 = BatchNormalization(name="encoder_bnorm2")
+    self.mpool2 = MaxPool2D(name='encoder_mpool2')
+
+    # 32 x 32 x 32
+    self.conv3 = Conv2D(filters=64, kernel_size=3, strides=1, padding='same', activation='relu', name='encoder_conv3')
+    self.bnorm3 = BatchNormalization(name="encoder_bnorm3")
+    self.mpool3 = MaxPool2D(name='encoder_mpool3')
+
+    # 16 x 16 x 64
+    self.conv4 = Conv2D(filters=128, kernel_size=3, strides=1, padding='same', activation='relu', name='encoder_conv4')
+    self.bnorm4 = BatchNormalization(name="encoder_bnorm4")
+    self.mpool4 = MaxPool2D(name='encoder_mpool4')
+
+    # 8 x 8 x 128
+    self.flatten = Flatten(name='encoder_flatten')
+
+    self.dense1 = Dense(2 * self.latent_dims, name='encoder_dense1')
+    # 2 x self.latent_dims
+
+
+    self.logvar = Dense(self.latent_dims, name='encoder_logvar')
+    self.mu = Dense(self.latent_dims, name='encoder_mu')
+    self.sigma = Lambda(lambda x: tf.exp(0.5*x), name='encoder_sigma')
+
+  def encode(self, inputs, preprocess=True):
+    # print the shape of the input
+    # print(f"Shape of input to encoder: {inputs.shape}")
+    p = self.preprocess(inputs) if preprocess else inputs
+    x=self.conv1(p); x=self.bnorm1(x); x=self.mpool1(x)
+    x=self.conv2(x); x=self.bnorm2(x);  x=self.mpool2(x)
+    x=self.conv3(x); x=self.bnorm3(x);  x=self.mpool3(x)
+    x=self.conv4(x); x=self.bnorm4(x);  x=self.mpool4(x)
+    x=self.flatten(x)
+    x=self.dense1(x)
+    # print(f"Shape of output from encoder: {x.shape}")
+
+    return self.mu(x), self.logvar(x), self.sigma(self.logvar(x)), p
+
+
+  def call(self, inputs):
+    return self.encode(inputs)
+
+  def summary(self, input_shape):
+    x = tf.keras.layers.Input(shape=input_shape)
+    model = tf.keras.Model(inputs=[x], outputs=self.call(x))
+    return model.summary()
+
+class Decoder(tf.keras.Model):
+    def __init__(self, latent_dims):
+        super().__init__(name='decoder')
+
+        self.latent_dims = latent_dims
+
+        # layers
+        self.dense1 = Dense(8 * 8 * 128, activation='relu', name='decoder_dense1')
+        self.reshape1 = Reshape((8, 8, 128), name='decoder_reshape1')
+
+        # 8 x 8 x 128 -> 16 x 16 x 64
+        self.convT1 = Conv2DTranspose(filters=64, kernel_size=3, strides=2, padding='same', activation='relu', name='decoder_convT1')
+        self.bnorm1 = BatchNormalization(name="decoder_bnorm1")
+
+        # 16 x 16 x 64 -> 32 x 32 x 32
+        self.convT2 = Conv2DTranspose(filters=32, kernel_size=3, strides=2, padding='same', activation='relu', name='decoder_convT2')
+        self.bnorm2 = BatchNormalization(name="decoder_bnorm2")
+
+        # 32 x 32 x 32 -> 64 x 64 x 16
+        self.convT3 = Conv2DTranspose(filters=16, kernel_size=3, strides=2, padding='same', activation='relu', name='decoder_convT3')
+        self.bnorm3 = BatchNormalization(name="decoder_bnorm3")
+
+        # 64 x 64 x 16 -> 128 x 128 x 1
+        self.convT4 = Conv2DTranspose(filters=1, kernel_size=3, strides=2, padding='same', activation='sigmoid', name='decoder_convT4')
+       
+
+    def decode(self, z):
+        x = self.dense1(z)
+        x = self.reshape1(x)
+
+        x = self.convT1(x); x = self.bnorm1(x)
+        x = self.convT2(x); x = self.bnorm2(x)
+        x = self.convT3(x); x = self.bnorm3(x)
+        x = self.convT4(x);
+
+        return x
+
+    def call(self, inputs):
+        return self.decode(inputs)
+
+    def summary(self, input_shape):
+        x = tf.keras.layers.Input(shape=input_shape)
+        model = tf.keras.Model(inputs=[x], outputs=self.call(x))
+        return model.summary()
+
+class ReconstructionLoss(tf.keras.layers.Layer):
+  def __init__(self):
+    super(ReconstructionLoss, self).__init__(name="reconstruction_loss")
+
+  def call(self, inputs):
+    x = inputs[0]
+    recon_x = inputs[1]
+
+    recon_loss = tf.reduce_sum(tf.keras.losses.binary_crossentropy(x, recon_x), axis=[1,2])
+    recon_loss = tf.reduce_mean(recon_loss)
+
+    self.add_loss(recon_loss)
+
+    return recon_loss
+
+class KLLoss(tf.keras.layers.Layer):
+  def __init__(self, beta=4.0):
+    super(KLLoss, self).__init__()
+
+    self.beta = beta
+
+  def call(self, inputs):
+    mu = inputs[0]
+    logvar = inputs[1]
+
+    kl_loss = -0.5 * tf.reduce_sum(1 + logvar - tf.square(mu) - tf.exp(logvar), axis=1)
+    kl_loss = kl_loss * self.beta
+
+    self.add_loss(kl_loss)
+    return kl_loss
+
+class Sampler(tf.keras.layers.Layer):
+  def __init__(self):
+    super().__init__(name="sampler")
+
+  def call(self, inputs):
+    mu = inputs[0]
+    sigma = inputs[1]
+
+    batch = tf.shape(mu)[0]
+    dim = tf.shape(mu)[1]
+    epsilon = tf.random.normal(shape=(batch, dim))
+
+    # reparameterize
+    output =  mu + tf.multiply(sigma, epsilon)
+
+    # print(f"Shape of output from sampler: {output.shape}")
+    return output
+
+class BVAE(tf.keras.Model):
+  def __init__(self, latent_dims):
+    super().__init__(name="bvae")
+
+    self.latent_dims = latent_dims
+
+    # layers
+    self.encoder = Encoder(self.latent_dims)
+    self.decoder = Decoder(self.latent_dims)
+
+    self.sampler = Sampler()
+
+    self.reconstruction_loss = ReconstructionLoss()
+    self.kl_loss = KLLoss(beta=4.0)
+
+    # trackers
+    self.loss_tracker = tf.keras.metrics.Mean(name="loss")
+    self.reconstruction_loss_tracker = tf.keras.metrics.Mean(name="reconstruction_loss")
+    self.kl_loss_tracker = tf.keras.metrics.Mean(name="kl_loss")
+
+  def get_config(self):
+    config = super().get_config()
+    config.update({
+        "latent_dims": self.latent_dims,
+    })
+    return config
+
+  @classmethod
+  def from_config(cls, config):
+    # Extract latent_dims from the config
+      latent_dims = config.pop('latent_dims')
+      # Remove other keys added by Keras during saving if they are not needed for __init__
+      config.pop('name', None)
+      config.pop('trainable', None)
+      config.pop('dtype', None)
+      # Create an instance of the class using the extracted latent_dims
+      return cls(latent_dims=latent_dims, **config)
+
+
+
+  def call(self, inputs):
+    mu, logvar, sigma, x = self.encoder(inputs)
+    z = self.sampler([mu, sigma])
+    # print(f"Shape of z: {z.shape}")
+    recon_x = self.decoder(z)
+
+    kl_loss = self.kl_loss([mu, logvar])
+    reconstruction_loss = self.reconstruction_loss([x, recon_x])
+
+    return recon_x, kl_loss, reconstruction_loss
+
+  @property
+  def metrics(self):
+      return [
+          self.loss_tracker,
+          self.reconstruction_loss_tracker,
+          self.kl_loss_tracker,
+      ]
+
+  def train_step(self, data):
+    # print(f"Shape of input data in train_step: {data.shape}")
+
+    with tf.GradientTape() as tape:
+      recon_x, kl_loss, reconstruction_loss = self(data)
+      total_loss = reconstruction_loss + kl_loss
+
+      # update the trackers
+      self.loss_tracker.update_state(total_loss)
+      self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+      self.kl_loss_tracker.update_state(kl_loss)
+
+      # gradients
+      grads = tape.gradient(total_loss, self.trainable_weights)
+      self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+    return {
+        m.name: m.result() for m in self.metrics
+    }
+
+  def test_step(self, data):
+    recon_x, kl_loss, reconstruction_loss = self(data)
+    total_loss = reconstruction_loss + kl_loss
+    self.loss_tracker.update_state(total_loss)
+    self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+    self.kl_loss_tracker.update_state(kl_loss)
+    return {
+        m.name: m.result() for m in self.metrics
+    }
+
+  def encode(self, inputs, preprocess=True):
+    return self.encoder.encode(inputs, preprocess=preprocess)
+
+  def decode(self, inputs):
+    return self.decoder.decode(inputs)
+
+  def sample(self, mu, sigma):
+    return self.sampler([mu, sigma])
+
+  def calculate_reconstruction_loss(self, inputs):
+    return self.reconstruction_loss(inputs)
+
+  def calculate_kl_loss(self, inputs):
+    return self.kl_loss(inputs)
--- gymnasium/PolyominoEnv.py
+++ gymnasium/PolyominoEnv.py
@@ -5,68 +5,11 @@ from itertools import cycle
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+from BVAE import BVAE
 
-from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
-import torch
-import torch.nn as nn
+latent_dimensions = 16
+bvae_model = None
 
-class CustomCNNExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space, features_dim=256):
-        super(CustomCNNExtractor, self).__init__(observation_space, features_dim)
-        
-        # CNN for processing images (assuming 128x128 grayscale images)
-        self.cnn = nn.Sequential(
-            nn.Conv2d(2, 32, kernel_size=3, padding=1),  # 2 channels for left+right
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1), 
-            nn.ReLU(),
-            nn.AdaptiveAvgPool2d((8, 8)),  # Reduce spatial dimensions
-            nn.Flatten()
-        )
-        
-        # Calculate CNN output size
-        cnn_output_size = 64 * 8 * 8  # 4096
-        
-        # Dense layers
-        self.fc = nn.Sequential(
-            nn.Linear(cnn_output_size + 2, 256),  # +2 for discrete features
-            nn.ReLU(),
-            nn.Linear(256, features_dim),
-            nn.ReLU()
-        )
-    
-    def forward(self, observations):
-        # Reshape pixel arrays to images (assuming 128x128)
-        left = observations['left'].view(-1, 1, 128, 128)
-        right = observations['right'].view(-1, 1, 128, 128)
-        
-        # Concatenate as 2-channel image
-        images = torch.cat([left, right], dim=1)
-        
-        # Process through CNN
-        cnn_features = self.cnn(images)  # Shape: [batch_size, 4096]
-        
-        # Get discrete features and ensure proper shape
-        last_action = observations['last_action_selection']
-        answered_correct = observations['answered_correct']
-        
-        # Handle different input shapes (squeeze extra dimensions if needed)
-        if last_action.dim() > 1:
-            last_action = last_action.squeeze(-1)
-        if answered_correct.dim() > 1:
-            answered_correct = answered_correct.squeeze(-1)
-            
-        discrete_features = torch.stack([
-            last_action.float(),
-            answered_correct.float()
-        ], dim=-1)  # Shape: [batch_size, 2]
-        
-        # Combine features (both should be 2D now)
-        combined = torch.cat([cnn_features, discrete_features], dim=-1)
-        
-        return self.fc(combined)
 class Actions(Enum):
     UP = 0
     DOWN = 1
@@ -118,21 +61,11 @@ class PolyominoEnvironment(gym.Env):
 
         self.latest_env_state = None
 
-        self.answered_correct = False
-        self.last_action_selection = False
+        self.answered = False
 
-        # self.observation_space = gym.spaces.Dict({
-        #     "left": gym.spaces.Box(low=0, high=255, shape=(16384,), dtype=np.float32),
-        #     "right": gym.spaces.Box(low=0, high=255, shape=(16384,), dtype=np.float32),
-        #     "last_action_selection": gym.spaces.Discrete(2),
-        #     "answered_correct": gym.spaces.Discrete(2)
-
-        # })
         self.observation_space = gym.spaces.Dict({
-            "left": gym.spaces.Box(low=0, high=255, shape=(16384,), dtype=np.float32),
-            "right": gym.spaces.Box(low=0, high=255, shape=(16384,), dtype=np.float32),
-            "last_action_selection": gym.spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32),
-            "answered_correct": gym.spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
+            "left": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(latent_dimensions,), dtype=np.float32),
+            "right": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(latent_dimensions,), dtype=np.float32),
         })
 
         self.context = zmq.Context()
@@ -184,7 +117,10 @@ class PolyominoEnvironment(gym.Env):
         end_time = time.time() + (timeout_ms / 1000)
 
         while time.time() < end_time:
-            topic, payload = self._recv()
+            try:
+                topic, payload = self._recv()
+            except zmq.Again:
+                continue
             # print(topic, payload)
             if "/state" in topic:
                 lastActionSeqNo = payload["data"]["last_action_seqno"]
@@ -200,40 +136,16 @@ class PolyominoEnvironment(gym.Env):
         return self.latest_env_state["isSame"] == selected_same
     
     def calculate_reward(self, action):
-        # if self.answered_correct:
-        #     if action != Actions.NEXT_SHAPE.value:
-        #         reward = -10
-        #     else:
-        #         reward = 10 
-        # elif action == Actions.NEXT_SHAPE.value:
-        #         reward = -25
-        # else:
-        #     if action in self.SELECTION_ACTIONS:
-        #         isCorrect = self._check_selection(action == Actions.SELECT_SAME.value)
-        #         if isCorrect:
-        #             reward = 10
-        #         else:
-        #             reward = -20
-        #     else:
-        #         reward = -1 
-        #
-        # promote trying different configurations to generalize better, todo
-        
+        #simplified for test
         if action in self.SELECTION_ACTIONS:
-            if self.answered_correct:
-                reward = -25
-            elif self.last_action_selection:
-                reward = -10
+            if self.answered:
+                reward = -1
             else:
                 isCorrect = self._check_selection(action == Actions.SELECT_SAME.value)
-                if isCorrect:
-                    self.answered_correct = True
-                reward = 35 if isCorrect else -50
+                reward = 10 if isCorrect else -20
+            self.answered = True
         else:
             reward = -1
-
-        if action == Actions.NEXT_SHAPE.value:
-            reward = 30 if self.answered_correct else -55
 
         return reward
 
@@ -251,19 +163,18 @@ class PolyominoEnvironment(gym.Env):
 
 
         left, right = self.latest_env_state["state"]
-        # print(len(left), len(right))
+        left = tf.convert_to_tensor(left, dtype=tf.float32)
+        right = tf.convert_to_tensor(right, dtype=tf.float32)
+        left, right = tf.reshape(left, (128, 128, 1)), tf.reshape(right, (128, 128, 1))
+        inputs = tf.stack([left, right], axis=0)  # Shape: (2, 128, 128, 1)
+        mu, _, _, _ = bvae_model.encode(inputs)
+        mu_left, mu_right = mu[0], mu[1]
 
-        self.answered_correct = False
-        self.last_action_selection = False
+        self.answered= False
 
-        left = np.array(left, dtype=np.float32)
-        right = np.array(right, dtype=np.float32)
-        
         observation = {
-            "left": left,
-            "right": right,
-            "last_action_selection": np.array([0], dtype=np.float32),
-            "answered_correct": np.array([0], dtype=np.float32)
+            "left": mu_left.numpy(),
+            "right": mu_right.numpy(),
         }
 
         info = {}
@@ -282,27 +193,21 @@ class PolyominoEnvironment(gym.Env):
 
         reward = self.calculate_reward(action)
 
-
-     
-        if action in self.SELECTION_ACTIONS:
-            self.last_action_selection = True
-        else:
-            self.last_action_selection = False
-
         if action == Actions.NEXT_SHAPE.value:
             self.current_problem += 1
-            self.answered_correct = False # reset after choosing the next shape
+            self.answered= False # reset after choosing the next shape
 
         left, right = self.latest_env_state["state"]
-
-        left = np.array(left, dtype=np.float32)
-        right = np.array(right, dtype=np.float32)
+        left = tf.convert_to_tensor(left, dtype=tf.float32)
+        right = tf.convert_to_tensor(right, dtype=tf.float32)
+        left, right = tf.reshape(left, (128, 128, 1)), tf.reshape(right, (128, 128, 1))
+        inputs = tf.stack([left, right], axis=0)  # Shape: (2, 128, 128, 1)
+        mu, _, _, _ = bvae_model.encode(inputs)
+        mu_left, mu_right = mu[0], mu[1]
 
         observation = {
-            "left": left,
-            "right": right,
-            "last_action_selection": np.array([1 if self.last_action_selection else 0], dtype=np.float32),
-            "answered_correct": np.array([1 if self.answered_correct else 0], dtype=np.float32)
+            "left": mu_left.numpy(),
+            "right": mu_right.numpy(),
         }
 
         info = response
@@ -329,29 +234,21 @@ are the actions hidden to the agent based on the playMode?; currently agent can 
 different approach to listener msgs.: use sqn no to sync the data
 """
 
-#
-# def main():
-#     env = PolyominoEnvironment()
-#     actions = [8, 0, 9]
-#     actions = cycle(actions)
-#     while True:
-#         action = next(actions)
-#         obs, reward, term, trun, info =  env.step(action)
-#         print(action, reward)
-#         time.sleep(2)
-#
 from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CheckpointCallback
 
 def train_model(training_model, training_steps = 100000, buffer_size = 30000, load_model = None):
+    global bvae_model
+    bvae_path = "./bvae_model_ldims_16_1750247228_new.keras"
+    bvae_model = tf.keras.models.load_model(bvae_path, custom_objects = {'BVAE': BVAE})
+    print(bvae_model.summary())
+    print("Setting latent dimensions", bvae_model.latent_dims)
+
+
     print("Testing with ", training_model.__name__, "for", training_steps, "steps")
 
-    policy_kwargs = dict(
-        features_extractor_class=CustomCNNExtractor,  
-        features_extractor_kwargs=dict(features_dim=256),
-    )
     env = PolyominoEnvironment()
     env = Monitor(env)
     checkpoint_callback = CheckpointCallback(
@@ -366,9 +263,8 @@ def train_model(training_model, training_steps = 100000, buffer_size = 30000, lo
         model = training_model.load(load_model, env=env, verbose=1)
     else:
         if training_model.__name__ == "DQN":
-            model = training_model("MultiInputPolicy", env, verbose=1, buffer_size=buffer_size, tensorboard_log="./tensorboard_logs/", policy_kwargs=policy_kwargs)
-        elif training_model.__name__ == "PPO":
-            model = training_model("MultiInputPolicy", env, verbose=1, ent_coef=0.001, tensorboard_log="./tensorboard_logs/", policy_kwargs=policy_kwargs)
+            model = training_model("MultiInputPolicy", env, verbose=1, buffer_size=buffer_size, tensorboard_log="./tensorboard_logs/")
+
 
     model.learn(total_timesteps=training_steps, callback=checkpoint_callback)
     model.save(f"model-{training_model.__name__}-{training_steps}")
@@ -404,4 +300,4 @@ if __name__ == "__main__":
     # latest_model_path = get_latest_model()
     # print(latest_model_path)
 
-    train_model(PPO, training_steps=500000, load_model=latest_model_path)
+    train_model(DQN, training_steps=500000, load_model=latest_model_path)
