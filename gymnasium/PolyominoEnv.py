af0215d28ea963d0a483a9bebf2439515108d64e
nao <theshishir01@gmail.com>
2025-06-19 19:05:43 -0500

    soc

--- gymnasium/PolyominoEnv.py
+++ gymnasium/PolyominoEnv.py
@@ -7,9 +7,6 @@ import tensorflow as tf
 import pandas as pd
 from BVAE import BVAE
 
-latent_dimensions = 16
-bvae_model = None
-
 class Actions(Enum):
     UP = 0
     DOWN = 1
@@ -24,7 +21,7 @@ class Actions(Enum):
     SELECT_DIFFERENT = 10
 
 class PolyominoEnvironment(gym.Env):
-    def __init__(self, PORT = 10002, LISTENER_PORT = 10001, HOST = 'localhost', TIMEOUT = 5000, MSG_TIMEOUT_FILTER = '', MAX_TIMESTEPS = 1000):
+    def __init__(self, PORT = 10002, LISTENER_PORT = 10001, HOST = 'localhost', TIMEOUT = 5000, MSG_TIMEOUT_FILTER = '', MAX_TIMESTEPS = 1000, BVAE_MODEL_PATH = "./bvae_model_ldims_16_1750247228_new.keras"):
         self.ACTION_MAP = {
               'W': 'up',
               'S': 'down',
@@ -38,6 +35,8 @@ class PolyominoEnvironment(gym.Env):
               '1': 'select_same_shape',
               '0': 'select_different_shape'
         }
+
+        self.bvae_model = self._load_bvae(BVAE_MODEL_PATH)
 
         self.ACTION_KEYS = list(self.ACTION_MAP.keys())
         self.ACTION_DESC = list(self.ACTION_MAP.values())
@@ -63,6 +62,7 @@ class PolyominoEnvironment(gym.Env):
 
         self.answered = False
 
+        latent_dimensions = self.bvae_model.latent_dims
         self.observation_space = gym.spaces.Dict({
             "left": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(latent_dimensions,), dtype=np.float32),
             "right": gym.spaces.Box(low=-np.inf, high=np.inf, shape=(latent_dimensions,), dtype=np.float32),
@@ -93,6 +93,12 @@ class PolyominoEnvironment(gym.Env):
         return {'header': header, 'data': data}
 
 
+    def _load_bvae(self, path):
+        bvae_model = tf.keras.models.load_model(path, custom_objects = {'BVAE': BVAE})
+        print(f"Loading Model with latent dimensions {bvae_model.latent_dims}")
+        print(bvae_model.summary())
+        return bvae_model
+
     def _send(self, data):
         self.seqno += 1
         request = self._create_request(data)
@@ -111,7 +117,15 @@ class PolyominoEnvironment(gym.Env):
         payload = json.loads(encoded_payload)
 
         return topic, payload
-        
+    
+    def _encode_state(self, left, right):
+        left = tf.convert_to_tensor(left, dtype=tf.float32)
+        right = tf.convert_to_tensor(right, dtype=tf.float32)
+        left, right = tf.reshape(left, (128, 128, 1)), tf.reshape(right, (128, 128, 1))
+        inputs = tf.stack([left, right], axis=0)  # Shape: (2, 128, 128, 1)
+        mu, _, _, _ = self.bvae_model.encode(inputs)
+        mu_left, mu_right = mu[0], mu[1]
+        return mu_left, mu_right
 
     def _wait_for_update(self, seqNo, timeout_ms=5000):
         end_time = time.time() + (timeout_ms / 1000)
@@ -160,15 +174,8 @@ class PolyominoEnvironment(gym.Env):
         self.current_timestep = 0
         self.current_problem = 0
 
-
-
         left, right = self.latest_env_state["state"]
-        left = tf.convert_to_tensor(left, dtype=tf.float32)
-        right = tf.convert_to_tensor(right, dtype=tf.float32)
-        left, right = tf.reshape(left, (128, 128, 1)), tf.reshape(right, (128, 128, 1))
-        inputs = tf.stack([left, right], axis=0)  # Shape: (2, 128, 128, 1)
-        mu, _, _, _ = bvae_model.encode(inputs)
-        mu_left, mu_right = mu[0], mu[1]
+        mu_left, mu_right = self._encode_state(left, right)
 
         self.answered= False
 
@@ -198,12 +205,7 @@ class PolyominoEnvironment(gym.Env):
             self.answered= False # reset after choosing the next shape
 
         left, right = self.latest_env_state["state"]
-        left = tf.convert_to_tensor(left, dtype=tf.float32)
-        right = tf.convert_to_tensor(right, dtype=tf.float32)
-        left, right = tf.reshape(left, (128, 128, 1)), tf.reshape(right, (128, 128, 1))
-        inputs = tf.stack([left, right], axis=0)  # Shape: (2, 128, 128, 1)
-        mu, _, _, _ = bvae_model.encode(inputs)
-        mu_left, mu_right = mu[0], mu[1]
+        mu_left, mu_right = self._encode_state(left, right)
 
         observation = {
             "left": mu_left.numpy(),
@@ -215,9 +217,6 @@ class PolyominoEnvironment(gym.Env):
         terminated = self.MAX_PROBLEMS <= self.current_problem
         truncated = False
         return observation, reward, terminated, truncated, info
-
-
-
 
     def close(self):
         self.socket.close()
@@ -234,70 +233,3 @@ are the actions hidden to the agent based on the playMode?; currently agent can 
 different approach to listener msgs.: use sqn no to sync the data
 """
 
-from stable_baselines3 import PPO, DQN
-from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.callbacks import CheckpointCallback
-
-def train_model(training_model, training_steps = 100000, buffer_size = 30000, load_model = None):
-    global bvae_model
-    bvae_path = "./bvae_model_ldims_16_1750247228_new.keras"
-    bvae_model = tf.keras.models.load_model(bvae_path, custom_objects = {'BVAE': BVAE})
-    print(bvae_model.summary())
-    print("Setting latent dimensions", bvae_model.latent_dims)
-
-
-    print("Testing with ", training_model.__name__, "for", training_steps, "steps")
-
-    env = PolyominoEnvironment()
-    env = Monitor(env)
-    checkpoint_callback = CheckpointCallback(
-        save_freq=10000,
-        save_path=f"./models/",
-        name_prefix=f"{training_model.__name__}_{training_steps}"
-    )
-    # model = PPO.load("./model-PPO-300000.zip")
-    check_env(env, warn=True)
-
-    if load_model:
-        model = training_model.load(load_model, env=env, verbose=1)
-    else:
-        if training_model.__name__ == "DQN":
-            model = training_model("MultiInputPolicy", env, verbose=1, buffer_size=buffer_size, tensorboard_log="./tensorboard_logs/")
-
-
-    model.learn(total_timesteps=training_steps, callback=checkpoint_callback)
-    model.save(f"model-{training_model.__name__}-{training_steps}")
-
-    obs, info = env.reset()
-    while True:
-        action, _ = model.predict(obs)
-        print(action)
-        obs, reward, term, trun, info = env.step(action)
-        print(reward)
-        # print(action, reward)
-        input()
-        if term or trun:
-            obs = env.reset()
-            break
-
-def get_latest_model():
-    model_dir = "./models/"
-    import os
-
-    files = [f for f in os.listdir(model_dir) if f.endswith('.zip')]
-    # sort them and get the latest one
-    files.sort(key=lambda x: os.path.getmtime(os.path.join(model_dir, x)), reverse=True)
-    if files:
-        return os.path.join(model_dir, files[0])
-    else:
-        return None
-
-
-if __name__ == "__main__":
-    latest_model_path = None
-
-    # latest_model_path = get_latest_model()
-    # print(latest_model_path)
-
-    train_model(DQN, training_steps=500000, load_model=latest_model_path)
---
+++ gymnasium/train_model.py
@@ -0,0 +1,69 @@
+from BVAE import BVAE
+from PolyominoEnv import PolyominoEnvironment
+import tensorflow as tf
+import numpy as np
+from stable_baselines3 import PPO, DQN
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import CheckpointCallback
+
+latent_dimensions = 16
+
+def train_model(training_model, training_steps = 100000, buffer_size = 30000, load_model = None):
+    print("Testing with ", training_model.__name__, "for", training_steps, "steps")
+
+    env = PolyominoEnvironment()
+    env = Monitor(env)
+    checkpoint_callback = CheckpointCallback(
+        save_freq=10000,
+        save_path=f"./models/",
+        name_prefix=f"{training_model.__name__}_{training_steps}"
+    )
+    # model = PPO.load("./model-PPO-300000.zip")
+    check_env(env, warn=True)
+
+    model = None
+
+    if load_model:
+        print("Loading Saved Model ...")
+        model = training_model.load(load_model, env=env, verbose=1)
+    else:
+        print("Setting up new model ...")
+        if training_model.__name__ == "DQN":
+            model = training_model("MultiInputPolicy", env, verbose=1, buffer_size=buffer_size, tensorboard_log="./tensorboard_logs/", exploration_fraction=0.5)
+
+
+    model.learn(total_timesteps=training_steps, callback=checkpoint_callback)
+    model.save(f"model-{training_model.__name__}-{training_steps}")
+
+    obs, info = env.reset()
+    while True:
+        action, _ = model.predict(obs)
+        print(action)
+        obs, reward, term, trun, info = env.step(action)
+        print(reward)
+        # print(action, reward)
+        input()
+        if term or trun:
+            obs = env.reset()
+            break
+
+def get_latest_model():
+    model_dir = "./models/"
+    import os
+
+    files = [f for f in os.listdir(model_dir) if f.endswith('.zip')]
+    # sort them and get the latest one
+    files.sort(key=lambda x: os.path.getmtime(os.path.join(model_dir, x)), reverse=True)
+    if files:
+        return os.path.join(model_dir, files[0])
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    latest_model_path = None
+
+    # latest_model_path = get_latest_model()
+    # print(latest_model_path)
+    train_model(DQN, training_steps=1000000, load_model=latest_model_path)
